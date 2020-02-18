ee6b809fc347b05c634a52ca90c0ec0a0cac8f59
reetssydney <ritaho.au@gmail.com>
2020-02-18 17:23:26 +0100

    updated js

--- newcomer-topics/assets/newcomertopics-init.js
+++ newcomer-topics/assets/newcomertopics-init.js
@@ -5,25 +5,21 @@ $(document).ready(function () {
     $('body').on('click','.topics-CTA', function(){
       $('#dialog1').toggleClass('showDialog hideDialog');
       $('#content').toggleClass('dimcontent');
-      $('.topics-CTA').attr("disabled", "true")
     });
 
     $('body').on('click','.closebtn', function(){
       $('#dialog1').toggleClass('showDialog hideDialog');
       $('#content').toggleClass('dimcontent');
-      $('.topics-CTA .intro-CTA').removeAttr("disabled")
     });
 
     $('body').on('click','.intro-CTA', function(){
       $('#dialog2').toggleClass('showDialog hideDialog');
       $('#content').toggleClass('dimcontent');
-      $('.topics-CTA .intro-CTA').attr("disabled", "true")
     });
 
     $('body').on('click','.closebtn2', function(){
       $('#dialog2').toggleClass('showDialog hideDialog');
       $('#content').toggleClass('dimcontent');
-      $('.topics-CTA .intro-CTA').removeAttr("disabled")
     });
 
 //    console.log("some text")
