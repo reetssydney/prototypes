9bfd8d0327b046d6991e9d085c1873e8b29fd363
rho <rho@wikimedia.org>
2021-08-02 16:14:54 +0200

    Created new prototype project

---
+++ Add image It1/public/js/data.js
@@ -0,0 +1,23 @@
+var church = {
+	title: 'Church',
+	description: 'Church order is the systematically organized set of rules drawn up by a qualified body for the church.',
+	text: 'church church Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+	imgName: 'Sofia kyrka.jpg',
+	imgDescription: 'Sofia Church in Stockholm. The description will wrap after a few more lines until it reaches the image heig...',
+	imgFile: '<img src="/img/church.jpg" />',
+	imgCaption: '',
+	nextTitle: 'croissant',
+	nextDescription: 'Type of pastry'
+}
+
+var croissant = {
+	title: 'Croissant',
+	description: 'A croissant is a buttery, flaky, viennoiserie pastry of Austrian origin, but mostly associated with France.',
+	text: 'A croissant (UK: /ˈkrwʌsɒ̃, ˈkrwæsɒ̃/,[3] US: /krəˈsɒnt, krwɑːˈsɒ̃/, French: [kʁwasɑ̃] (About this soundlisten)) is a buttery, flaky, viennoiserie pastry of Austrian origin,[1] but mostly associated with France. Croissants are named for its historical crescent shape and, like other viennoiserie, are made of a layered yeast-leavened dough. The dough is layered with butter, rolled and folded several times in succession, then rolled into a thin sheet, in a technique called laminating. The process results in a layered, flaky texture, similar to a puff pastry.',
+	imgName: 'APileOfCroissants.jpg',
+	imgDescription: 'A pile of croissants',
+	imgFile: '<img src="/img/croissant.jpg" />',
+	imgCaption: '',
+	nextTitle: 'sun',
+	nextDescription: 'star at the centre of the Solar System'
+}
